b2217d5754a347db0d391a3acf4cba51374057f0
regzimarx <gergimarjohnalinsangao@gmail.com>
2017-09-09 19:23:30 +0800

    Add list of profiles

--- management/views.py
+++ management/views.py
@@ -1,3 +1,18 @@
+from django.utils import timezone
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+from users.models import User
+
+class ProfilesListView(TemplateView):
+    """ View for list of all profiles """
+
+    template_name = 'management/list.html'
+
+    def get_context_data(self):
+        context = super(ProfilesListView, self).get_context_data()
+        context['profiles'] = User.objects.filter(
+            is_staff=False,
+            date_joined__year=timezone.now().year
+        )
+        return context
--- torch/urls.py
+++ torch/urls.py
@@ -3,5 +3,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'', include('users.urls', namespace='account')),
+    url(r'', include('management.urls', namespace='management')),
     url(r'^admin/', admin.site.urls),
 ]
